122d2a33ed5a94dd26f8ebadfec3029849440540
Gabriel Mendonça <gabrielmendoncanf@gmail.com>
2016-07-08 15:01:45 -0300

    Adicionando try catch para não emitir exception quando não existir o arquivo .env

--- config.php
+++ config.php
@@ -5,8 +5,10 @@ define('DS', DIRECTORY_SEPARATOR);
 
 require PATH_ROOT . DS . 'vendor' . DS .'autoload.php'; 
 
-$dotenv = new Dotenv\Dotenv(__DIR__);
-$dotenv->load();
+try{
+	$dotenv = new Dotenv\Dotenv(__DIR__);
+	$dotenv->load();	
+}catch(Exception $e){}
 
 define("FACEBOOK_APP_ID", getenv('FACEBOOK_APP_ID')); 
 define("FACEBOOK_SECRED_KEY", getenv("FACEBOOK_SECRED_KEY"));
